019ebc7b809253f404bcc837afeee39505179383
Jiachen Chen <chenjiachen@me.com>
2021-04-11 14:22:04 -0700

    Update BAHAMUT Auto Dark Mode

--- BAHAMUT Auto Dark Mode.user.js
+++ BAHAMUT Auto Dark Mode.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name                 BAHAMUT Auto Dark Mode
 // @namespace            http://tampermonkey.net/
-// @version              0.1
+// @version              0.2
 // @description          Turn on/off dark mode automatically in ani.gamer.com.tw based on the color scheme of OS.
 // @description:ZH-CN    根据系统配色自动开启/关闭 ani.gamer.com.tw 的深色模式
 // @author               Jiachen Chen
@@ -12,22 +12,22 @@
     "use strict";
 
     const htmlTag = document.getElementsByTagName("html")[0];
-    const currentTheme = htmlTag.getAttribute("data-theme");
+    const lightSwitch = document.getElementById("lightSwitch");
 
     if (
         window.matchMedia &&
         window.matchMedia("(prefers-color-scheme: light)").matches
     ) {
-        if (currentTheme === "dark") {
-            htmlTag.setAttribute("data-theme", "light");
+        if (htmlTag.getAttribute("data-theme") === "dark") {
+            lightSwitch.click();
         }
     }
 
     window
         .matchMedia("(prefers-color-scheme: light)")
         .addEventListener("change", (e) => {
-            if (currentTheme === "dark") {
-                htmlTag.setAttribute("data-theme", "light");
+            if (htmlTag.getAttribute("data-theme") === "dark") {
+                lightSwitch.click();
             }
         });
 
@@ -35,16 +35,16 @@
         window.matchMedia &&
         window.matchMedia("(prefers-color-scheme: dark)").matches
     ) {
-        if (currentTheme === "light") {
-            htmlTag.setAttribute("data-theme", "dark");
+        if (htmlTag.getAttribute("data-theme") === "light") {
+            lightSwitch.click();
         }
     }
 
     window
         .matchMedia("(prefers-color-scheme: dark)")
         .addEventListener("change", (e) => {
-            if (currentTheme === "light") {
-                htmlTag.setAttribute("data-theme", "dark");
+            if (htmlTag.getAttribute("data-theme") === "light") {
+                lightSwitch.click();
             }
         });
 })();
